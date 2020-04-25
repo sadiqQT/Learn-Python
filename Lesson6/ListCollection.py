0bb35f3ad105c553f3b972c943a576f3706fc2c7
sadiqQT <47677625+sadiqQT@users.noreply.github.com>
2020-04-25 11:30:59 -0400

    Lesson#6 Collection

---
+++ Lesson6/ListCollection.py
@@ -0,0 +1,10 @@
+# Collection : How to use List
+# Find the length of List's items . In other words, total number of items(data) in a List 
+try:
+    Names = ["Ahmed", "Amir","Sadiq", "Ahmed" , "Humza" , "Haris"]
+    print( len(Names) )
+
+except Exception:
+    print("Sorry, program encounter an error")
+finally:
+    print("Nice! Program for learning Collection executed successfully!")
